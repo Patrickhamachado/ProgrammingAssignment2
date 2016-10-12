310f1d209fc1b1ba4796bd3c3a3c4e6a437b6d48
Patrickhamachado <patrickhamachado@gmail.com>
2016-10-12 13:11:56 -0500

    Assignment for week 3 : Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions can be used to calculate the inverse of a Matrix
+## 1- Create an object this way: TempObject = makeCacheMatrix ( YourData )
+## 2- Calculate the inverse matrix : InverseM = cachesolve(TempObject)
+## 3- For changing the data, execute: matx$set( NewData )
+## 4- Calculate the inverse matrix : InverseM = cachesolve(TempObject)
 
-## Write a short comment describing this function
+## --------------------- makeCacheMatrix --------------------------------
 
-makeCacheMatrix <- function(x = matrix()) {
+# This first function creates a special "matrix" object that can cache 
+# its inverse
 
+# makeCacheMatrix function builds a list of functions 
+# with two internal variables:
+# matr : the data matrix
+# minv : the inverse of the data matrix. Starts like NULL and becomes NULL 
+#           when MATR changes, with the SET function 
+
+makeCacheMatrix <- function( matr = matrix()  )  {
+    minv <- NULL
+    set <- function(y) {
+        matr <<- y
+        minv <<- NULL
+    }
+    get <- function() matr
+    setminv <- function(minv_new) minv <<- minv_new
+    getminv <- function() minv
+    list(set = set, get = get,
+         setminv = setminv,
+         getminv = getminv)
 }
 
+## ------------------------ cachesolve ------------------------------
 
-## Write a short comment describing this function
+# This second function computes the inverse of the special "matrix"
+# returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix 
+# has not changed), then the cachesolve should retrieve the 
+# inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cachesolve function calculates de inverse matrix, if it doesn't exist.
+# If exists, returns the last value, without calculating it again 
+
+cachesolve <- function(matrli , ... ) {
+    minv <- matrli$getminv()
+    if(!is.null(minv) ) {
+        message("Inverse matrix hasn't changed. Getting cached data")
+        return(minv)
+    }
+    data <- matrli$get() 
+    minv <- solve(data , ... )
+    matrli$setminv(minv)
+    minv
 }
+
+## --------------------------   Tests  ------------------------------------
+# Tests
+# matx = makeCacheMatrix ( matrix(rnorm(4) ,2 , 2) )
+# matx = makeCacheMatrix ( )
+# matx$set(matrix(rnorm(4) ,2 , 2) )
+# cachesolve(matx)
